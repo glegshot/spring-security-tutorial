aa26ab15907f0db96dbc30c5dbe56b775f96e753
glegshot <sreekumar.y2k@gmail.com>
2021-02-13 16:18:44 +0530

    working on jpa based authentication

--- src/main/java/org/example/config/AppSecurityConfiguration.java
+++ src/main/java/org/example/config/AppSecurityConfiguration.java
@@ -1,24 +1,43 @@
 package org.example.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class AppSecurityConfiguration extends WebSecurityConfigurerAdapter {
+/*
+    @Autowired
+    DataSource dataSource;*/
+
+    @Autowired
+    UserDetailsService userDetailsService;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
-        auth.inMemoryAuthentication().withUser("sree").password("123").roles("USER")
+        /*auth.inMemoryAuthentication().withUser("sree").password("123").roles("USER")
                 .and()
-                .withUser("sk").password("234").roles("ADMIN");
+                .withUser("sk").password("234").roles("ADMIN");*/
+
+      /*  auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .withDefaultSchema()
+                .withUser(User.withUsername("user").password("pass").roles("USER"))
+                .withUser(User.withUsername("admin").password("pass").roles("ADMIN"));*/
+
+         auth.userDetailsService(userDetailsService);
 
     }
 
